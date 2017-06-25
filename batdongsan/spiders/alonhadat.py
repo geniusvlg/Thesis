caf1079105a37e8681872370bd2bfd8f8460e5fd
Unknown <geniusvlg@yahoo.com.vn>
2017-06-25 11:31:08 +0700

    New setting for alonhadat spider

--- batdongsan/spiders/alonhadat.py
+++ batdongsan/spiders/alonhadat.py
@@ -9,6 +9,7 @@ from scrapy.selector import Selector
 
 class AlonhadatSpider(scrapy.Spider):
 	name = 'alonhadat'
+	words = []
 
 	def start_requests(self):		
 		self.is_updated = False 
@@ -154,57 +155,30 @@ class AlonhadatSpider(scrapy.Spider):
 		# Get title
 		title = self.convert_unicode(response.xpath('//div[@class="title"]/h1/text()').extract_first())
 
-		# Get location
-		location = response.xpath("//div[@class='address']/span[@class='value']/text()").extract_first()
-		location = self.convert_unicode(location)
-		location_detail = location
-		location_list = location.split(',')
-
 		# Get description
 		description = response.xpath("//div[contains(@class,'detail')]//text()").extract()
 		description = '-'.join(description)
 		description = self.convert_unicode(description)
 
-		if len(location_list) > 3:
-			# Get road
-			road = location_list[0]
+		# Get location
+		loca_list = response.xpath("//span[@itemprop='name']")
+		redundant_word = response.xpath("//span[@itemprop='name']/text()")[2].extract()
+		road = response.xpath("//span[@itemprop='name']/text()")[6].extract()
+		road = self.convert_unicode(road.replace(redundant_word, ""))
 
-			# Get ward
-			ward = location_list[1]
+		ward = response.xpath("//span[@itemprop='name']/text()")[5].extract()
+		ward = self.convert_unicode(ward.repalce(redundant_word, ""))
 
-			# Get county
-			county = location_list[2]
+		county = response.xpath("//span[@itemprop='name']/text()")[4].extract()
+		county = self.convert_unicode(county.repalce(redundant_word, ""))
 
-			# Get province
-			province = location_list[3]
-
-		elif len(location_list) > 2:
-			road = ''
-
-			# Get ward
-			ward = location_list[0]
-
-			# Get county
-			county = location_list[1]
-
-			# Get province
-			province = location_list[2]
-
-		else:
-			road = ""
-			ward = ""
-
-			# Get county
-			county = location_list[0]
-
-			# Get province
-			province = location_list[1]
+		province = response.xpath("//span[@itemprop='name']/text()")[3].extract()
+		province = self.convert_unicode(province.repalce(redundant_word, ""))
 
 		road = road.strip()
 		ward = ward.strip()
 		county = county.strip()
 		province = province.strip()
-
 
 		# Get author name
 		author = response.xpath("//span[@class='name']/span[@class='value']/text()").extract_first()
